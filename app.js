002da3f7a353f6df0ce13a4c09546aafa6a94b6c
bartekb4 <bartek4566@gmail.com>
2020-01-19 22:05:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const request = require('request');
 var pg = require('pg');
 //const engine = require(__dirname + "/engine.js");    //module
-var connect = "postgres://lolz:321654@localhost/BD_PROJEKT"
+var connect = 'postgresql://postgres:password@localhost/leagueproj'
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -11,12 +11,32 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.use(bodyParser.json());
 
+app.use(function(req,res,next){
+    res.locals.userValue = null;
+    next();
+})
+
+app.post('/team/add',function(req,res){
+    var team = {
+        first : req.body.fname,
+    }
+    console.log(team);
+    res.render('index',{
+        userValue : team
+    });
+    //res.json(student);
+     
+});
+
 app.get("/", async function(req,res){
-    const { Pool, Client } = require('pg')
-  const connectionString = 'postgresql://OZ:123456@localhost/BD_PROJEKT'
+	var test;
+  const { Pool, Client } = require('pg')
+  const connectionString = 'postgresql://postgres:password@localhost/leagueproj'
   const pool = new Pool({
     connectionString: connectionString,
+	
   })
+  
   pool.query('select * from playersdb', (err, res) => {
   //  console.log(err, res)
     pool.end()
@@ -29,8 +49,15 @@ app.get("/", async function(req,res){
   //  console.log(err, res)
     client.end()
   })
-  res.render('index');
+  
+    res.render('index', {
+        test: test
+    });
+	
+	
+  
 });
+
 
 app.listen(3000, function(){
   console.log("Server started on port 3000...........................................................................................................................");
