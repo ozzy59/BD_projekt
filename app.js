6695f10f1a9f82db7a1fc4bbe3b5e44c5560d770
bartekb4 <bartek4566@gmail.com>
2020-01-20 14:37:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -11,41 +11,32 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.use(bodyParser.json());
 var pg = require('pg');
-global.name;
+global.name2='testin';
 
 app.use(function(req,res,next){
     res.locals.userValue = null;
     next();
 })
 
-
 app.post('/team/add',function(req,res){
     var team = {
         first : req.body.fname,
     }
+	name2=team.first;
     console.log(team);
     res.render('index',{
         userValue : team
     });
     //res.json(student);
-     
+     console.log(name2);
 });
 app.get("/create", async function(req,res){    //tworze druzyne podana przez usera
-	//var name='user3';
-	const readline = require('readline').createInterface({
-	  input: process.stdin,
-	  output: process.stdout
-	})
-	
-	readline.question(`Enter team name?`, (name) => {
-    console.log(`Hi ${name}!`)
-    readline.close()                                              //te linie do czytania z konsoli
-	
 
+	console.log(name2);
 	var conString = "postgres://postgres:pass@localhost:5432/league";
 	
 	var client = new pg.Client(conString);
-	var sql='create table '+name+' (id integer UNIQUE, name varchar(20) primary key, team varchar(3),position varchar(3), cost decimal, total_points integer, FOREIGN KEY (id, total_points) references playersdb(id, total_points) on update cascade);' 
+	var sql='create table '+name2+' (id integer UNIQUE, name varchar(20) primary key, team varchar(3),position varchar(3), cost decimal, total_points integer, FOREIGN KEY (id, total_points) references playersdb(id, total_points) on update cascade);' 
 
 	client.connect();
 	client.query(sql, (err, res) => {
@@ -54,14 +45,15 @@ app.get("/create", async function(req,res){    //tworze druzyne podana przez use
 	client.end()
 	});
 	res.render('index',{
-        userValue : name
+        userValue : res
     });
-})
+//})
 	
 });
 app.get("/insert", async function(req,res){  //robie insert pilkarza podanego przez usera
 	var team='arsenal';
 	var player;
+	console.log(name2);
 	const readline = require('readline').createInterface({
 	  input: process.stdin,
 	  output: process.stdout
@@ -69,13 +61,13 @@ app.get("/insert", async function(req,res){  //robie insert pilkarza podanego pr
 	
 	readline.question(`Enter player name name?`, (player) => {
 	
-    console.log('${player}')
+    
     readline.close()
 	
 	var conString = "postgres://postgres:pass@localhost:5432/league";
 	
 	var client = new pg.Client(conString);
-	var sql="insert into "+team+" select * from playersdb where name='"+player+"';"
+	var sql="insert into "+name2+" select * from playersdb where name='"+player+"';"
 
 	client.connect();
 	client.query(sql, (err, res) => {
@@ -86,26 +78,17 @@ app.get("/insert", async function(req,res){  //robie insert pilkarza podanego pr
 		res.render('index',{
         userValue : team
     });
-})
 
-	
+
+	})
 });
 app.get("/list", async function(req,res){    //wypisuje druzyne usera
 	//var name='user3';
-	const readline = require('readline').createInterface({
-	  input: process.stdin,
-	  output: process.stdout
-	})
-	
-	readline.question(`Enter team name?`, (name) => {
-    console.log(`Hi ${name}!`)
-    readline.close()
-	
 
 	var conString = "postgres://postgres:pass@localhost:5432/league";
 	
 	var client = new pg.Client(conString);
-	var sql='select * from '+name+';' 
+	var sql='select * from '+name2+';' 
 
 	client.connect();
 	client.query(sql, (err, res) => {
@@ -116,7 +99,7 @@ app.get("/list", async function(req,res){    //wypisuje druzyne usera
 	res.render('index',{
         userValue : name
     });
-})
+
 	
 });
 
