5b50e17f225f2f2fbe9401e9042e781b8c030d69
bartekb4 <bartek4566@gmail.com>
2020-01-23 10:07:07 +0100

    zapytanie do listowania zawodnikow, wypisuje sie w konsoli

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const request = require('request');
 var pg = require('pg');
 //const engine = require(__dirname + "/engine.js");    //module
-var conString = "postgres://postgres:password@localhost:5432/leagueproj";
+var conString = "postgres://postgres:pass@localhost:5432/league";
 //var connect = 'postgresql://OZ:123456@localhost/league';
 const app = express();
 
@@ -41,13 +41,13 @@ app.post('/stworzDruzyne',function(req,res){
 
 });
 app.post("/addTeam", async function(req,res){    //tworze druzyne podana przez usera
-	var conString = "postgres://postgres:password@localhost:5432/leagueproj";
+	var conString = "postgres://postgres:pass@localhost:5432/league";
 	console.log(req.body.teamName);
 	var name=req.body.teamName;
 	var client = new pg.Client(conString);
 	 client.connect();
 	 const text = "create table "+name+" (id integer UNIQUE, name varchar(20) primary key, team varchar(3),position varchar(3), cost decimal, total_points integer, FOREIGN KEY (id, total_points) references playersdb(id, total_points) on update cascade);"+"create function check_budget_"+name+"() returns trigger as '"+ "Begin if NEW.cost +(select sum(cost) from "+name+")>100 then raise notice ''Budget exceeded''; return null; "
-+"else return new; end if; end; 'Language 'plpgsql';"+"create trigger budget_trigger before insert on "+name+" for each row execute procedure check_budget();"
++"else return new; end if; end; 'Language 'plpgsql';"+"create trigger budget_trigger"+name+" before insert on "+name+" for each row execute procedure check_budget();"
 	const values = [req.body.teamName]
 // callback
 	client.query(text, (err, res) => {
@@ -67,7 +67,7 @@ app.post("/addTeam", async function(req,res){    //tworze druzyne podana przez u
 	res.redirect('/wyborZawodnika');
 });
 app.post("/addPlayer", async function(req,res){    //tworze druzyne podana przez usera
-	var conString = "postgres://postgres:password@localhost:5432/leagueproj";
+	var conString = "postgres://postgres:pass@localhost:5432/league";
 	console.log(req.body.plName);
 	var pname=req.body.plName;
 	var tname = req.body.teamName;
@@ -92,7 +92,7 @@ app.post("/addPlayer", async function(req,res){    //tworze druzyne podana przez
 app.get("/", async function(req,res){
 	var name;
 
-	var conString = "postgres://postgres:password@localhost:5432/leagueproj";
+	var conString = "postgres://postgres:pass@localhost:5432/league";
  // var conString = "postgres://OZ:123456@localhost:5432/league";
 
 	var client = new pg.Client(conString);
@@ -111,15 +111,23 @@ app.get("/", async function(req,res){
     });
 
 });
-app.get("/list", async function(req,res){    //tworze druzyne podana przez usera
-	var conString = "postgres://postgres:password@localhost:5432/leagueproj";
-	console.log(req.body.plName);
-	var pname=req.body.plName;
-	var tname = req.body.teamName;
+app.post("/list", async function(req,res){    //tworze druzyne podana przez usera
+	//var conString = "postgres://postgres:password@localhost:5432/leagueproj";
+	var conString = "postgres://postgres:pass@localhost:5432/league";
+
 	var client = new pg.Client(conString);
+	console.log(req.body.cost);
+	console.log(req.body.team);
+	console.log(req.body.pos);
 	 client.connect();
+	 var name=req.body.playerName;
+	 var cena=req.body.cost;
+	 var team=req.body.team;
+	 var pos=req.body.pos;
+
 	
-	const text= "select * from bartoszbryk"  ///ni chuja nie wiem jak wypisac tych zawodników
+	const text= "select * from playersdb where name='"+name+"' or cost>"+cena+" and team='"+team+"' and position='"+pos+"';" ///ni chuja nie wiem jak wypisac tych zawodników
+	//const values=[req.body.Cena, req.body.teamName, req.body.Pozycja];
 	//zamia
 // callback
 	client.query(text, (err, response) => {
