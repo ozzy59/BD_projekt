023c586f7188c52dbf146415da48b8b7e8c8954c
Sergiusz-Okarmus <osergio@interia.pl>
2020-01-23 15:30:01 +0100

    done

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 var pg = require('pg');
+const engine = require(__dirname + "/engine.js");
 //const engine = require(__dirname + "/engine.js");    //module
-var conString = "postgres://postgres:pass@localhost:5432/league";
-//var connect = 'postgresql://OZ:123456@localhost/league';
+// var conString = "postgres://postgres:pass@localhost:5432/league";
+var conString = 'postgresql://OZ:123456@localhost/league';
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -12,7 +13,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.use(bodyParser.json());
 
-var teamName = "coksaock";
+var teamName;
+var newTeamName;
 
 app.use(function(req,res,next){
     res.locals.userValue = null;
@@ -28,104 +30,40 @@ app.post('/',function(req,res){
 
 });
 
+app.post('/addTeam', function(req,res){
+  newTeamName = req.body.teamName;
+  res.redirect("/");
+});
+
 app.post('/wybórZawodnika',function(req,res){
     teamName = req.body.teamName;
     res.redirect("/wyborZawodnika");
 
 });
 
-
-app.post('/stworzDruzyne',function(req,res){
-	res.render("create")
-    res.redirect("/create");
-
+app.post("/create", function(req,res){
+  res.redirect("/create");
 });
-app.post("/addTeam", async function(req,res){    //tworze druzyne podana przez usera
-	var conString = "postgres://postgres:pass@localhost:5432/league";
-	console.log(req.body.teamName);
-	var name=req.body.teamName;
-	var client = new pg.Client(conString);
-	 client.connect();
-	 const text = "create table "+name+" (id integer UNIQUE, name varchar(20) primary key, team varchar(3),position varchar(3), cost decimal, total_points integer, FOREIGN KEY (id, total_points) references playersdb(id, total_points) on update cascade);"+"create function check_budget_"+name+"() returns trigger as '"+ "Begin if NEW.cost +(select sum(cost) from "+name+")>100 then raise notice ''Budget exceeded''; return null; "
-+"else return new; end if; end; 'Language 'plpgsql';"+"create trigger budget_trigger"+name+" before insert on "+name+" for each row execute procedure check_budget();"
-	const values = [req.body.teamName]
-// callback
-	client.query(text, (err, res) => {
-	  if (err) {
-		console.log(err.stack)
-	  } else {
-		console.log(res)
-		
-	  }
 
-	  client.end();
-	
-	})
-
-	
-
-	res.redirect('/wyborZawodnika');
+app.get("/create", function(req,res){
+  res.render("./create");
 });
-app.post("/addPlayer", async function(req,res){    //tworze druzyne podana przez usera
-	var conString = "postgres://postgres:pass@localhost:5432/league";
-	console.log(req.body.plName);
-	var pname=req.body.plName;
-	var tname = req.body.teamName;
-	var client = new pg.Client(conString);
-	 client.connect();
-	
-	const text= "insert into "+tname+" (select * from playersdb where name='"+pname+"');"  
-	//zamia
-// callback
-	client.query(text, (err, res) => {
-	  if (err) {
-		console.log(err.stack)
-	  } else {
-		console.log(res)
 
-	  }
-	  client.end();
-	})
-
-	res.redirect('/wyborZawodnika');
-});
-app.post("/removePlayer", async function(req,res){    //tworze druzyne podana przez usera
-	var conString = "postgres://postgres:pass@localhost:5432/league";
-	console.log(req.body.plName);
-	var pnamedel=req.body.plNameDel;
-	var tnamedel = req.body.teamNameDel;
-	var client = new pg.Client(conString);
-	 client.connect();
-	
-	const text= "delete from "+tnamedel+" where name='"+pnamedel+"'";
-	//zamia
-// callback
-	client.query(text, (err, res) => {
-	  if (err) {
-		console.log(err.stack)
-	  } else {
-		console.log(res)
-
-	  }
-	  client.end();
-	})
-
-	res.redirect('/wyborZawodnika');
-});
 app.get("/", async function(req,res){
+  if(newTeamName != null){ engine.createTeam(newTeamName); teamName = newTeamName;}
+
+  newTeamName = null;
 	var name;
 
-	var conString = "postgres://postgres:pass@localhost:5432/league";
- // var conString = "postgres://OZ:123456@localhost:5432/league";
+	// var conString = "postgres://postgres:pass@localhost:5432/league";
+ var conString = "postgres://OZ:123456@localhost:5432/league";
 
 	var client = new pg.Client(conString);
 	client.connect();
 
-	client.query('SELECT * from playersdb').then(res => {
+	client.query("SELECT * from playersdb").then(res => {
 	 // console.log(err ? err.stack : res.rows[0].name) // Hello World!
-
    const result = (res.rows);
-
        // console.log(result);
 	}).finally(() => client.end());
 
@@ -134,40 +72,29 @@ app.get("/", async function(req,res){
     });
 
 });
-app.post("/list", async function(req,res){    //tworze druzyne podana przez usera
-	//var conString = "postgres://postgres:password@localhost:5432/leagueproj";
-	var conString = "postgres://postgres:pass@localhost:5432/league";
 
-	var client = new pg.Client(conString);
-	console.log(req.body.cost);
-	console.log(req.body.team);
-	console.log(req.body.pos);
-	 client.connect();
-	 var name=req.body.playerName;
-	 var cena=req.body.cost;
-	 var team=req.body.team;
-	 var pos=req.body.pos;
-
-	
-	const text= "select * from playersdb where name='"+name+"' or cost>"+cena+" and team='"+team+"' and position='"+pos+"';" ///ni chuja nie wiem jak wypisac tych zawodników na stronie
-	//const values=[req.body.Cena, req.body.teamName, req.body.Pozycja];
-	//zamia
-// callback
-	client.query(text, (err, response) => {
-	  if (err) {
-		console.log(err.stack)
-	  } else {
-		console.log(response.rows)
-	
-	  }
-	 
-	  client.end();
-	})
-
-	res.redirect('/wyborZawodnika');
+app.post("/removePlayer", function(req,res){
+  engine.removePlayer(teamName, req.body.plNameDel);
+  res.redirect("./wyborZawodnika");
 });
+
+app.post("/addPlayer", function(req,res){
+  engine.addPlayer(teamName, req.body.plName);
+  res.redirect("./wyborZawodnika");
+});
+
+app.post("/list", function(req,res){    //filtruje baze
+  engine.getFilteredList(req.body.pos);
+  res.redirect('/wyborZawodnika');
+});
+
+app.post("/usersList", function(req,res){
+  engine.getUsersList(teamName);
+  res.redirect('/wyborZawodnika');
+});
+
 app.get("/wyborZawodnika", async function(req,res){
-  res.render('wyborZawodnika', {team: teamName});
+  res.render('./wyborZawodnika', {team: teamName});
 });
 
 app.listen(3000, function(){
--- engine.js
+++ engine.js
@@ -1,58 +1,100 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+var pg = require('pg');
+const conString = 'postgresql://OZ:123456@localhost/league';
 global.name2='testin';
 
-exports.getUsersList = function(){ // zwraca druzyne pilkarza
-  var conString = 'postgresql://OZ:123456@localhost/league'
+exports.addPlayer = function(teamName, plName){
+  // var conString = "postgres://postgres:pass@localhost:5432/league";
+  // var conString = 'postgresql://OZ:123456@localhost/league';
+	console.log(plName);
+	var client = new pg.Client(conString);
+	 client.connect();
+
+	const text= "insert into "+ teamName +" (select * from playersdb where name='"+plName+"');"
+	//zamia
+// callback
+	client.query(text, (err, res) => {
+	  if (err) {
+		console.log(err.stack)
+	  } else {
+		console.log(res)
+	  }
+	  client.end();
+	})
+}
+
+exports.removePlayer = function(teamName, playersName){
+  //var conString = "postgres://postgres:password@localhost:5432/leagueproj";
+  // var conString = 'postgresql://OZ:123456@localhost/league';
 
   var client = new pg.Client(conString);
-  var sql='select * from '+name2+';'
+   client.connect();
+
+  const text= "delete from "+teamName+" where name = '"+playersName+"';" ///ni chuja nie wiem jak wypisac tych zawodników na stronie
+  //const values=[req.body.Cena, req.body.teamName, req.body.Pozycja];
+  //zamia
+// callback
+  client.query(text, (err, response) => {
+    if (err) {
+    console.log(err.stack)
+    } else {
+    console.log(response.rows)
+
+    }
+
+    client.end();
+  })
+}
+
+exports.getFilteredList = function(position){ // zwraca druzyne pilkarza
+  // var conString = 'postgresql://OZ:123456@localhost/league';
+  // console.log(position);
+  var client = new pg.Client(conString);
+  var sql="select * from playersdb where position = '"+position+"';"
 
   client.connect();
-  client.query(sql, (err, res) => {
-    console.log(err ? err.stack : res.rows) // Hello World!
+  client.query(sql).then(res => {
+    const result = (res.rows);
+    // console.log(result);
+    // console.log(err ? err.stack : res.rows) // Hello World!
   client.end()
   });
 }
 
-exports.insertPlayer = function(team){ //insert pilkarza do druzyny
-	var player;
-	console.log(name2);
-	const readline = require('readline').createInterface({
-	  input: process.stdin,
-	  output: process.stdout
-	})
+exports.getUsersList = function(teamName){ // zwraca druzyne pilkarza
+  // var conString = 'postgresql://OZ:123456@localhost/league';
 
-	readline.question(`Enter player name name?`, (player) => {
+  var client = new pg.Client(conString);
+  var sql="select * from "+teamName+";"
 
-
-    readline.close()
-
-	// var conString = "postgres://postgres:pass@localhost:5432/league";
-	var conString = 'postgresql://OZ:123456@localhost/league'
-	var client = new pg.Client(conString);
-	var sql="insert into "+name2+" select * from playersdb where name='"+player+"';"
-
-	client.connect();
-	client.query(sql, (err, res) => {
-	  console.log(err ? err.stack : res) // Hello World!
-
-	client.end()
-	});
-	})
+  client.connect();
+  client.query(sql).then(res => {
+    const result = (res.rows);
+    console.log(result);
+    // console.log(err ? err.stack : res.rows) // Hello World!
+  client.end()
+  });
 }
 
-exports.createTeam = function(){
-  console.log(name2);
-  // var conString = "postgres://postgres:pass@localhost:5432/league";
-	var conString = 'postgresql://OZ:123456@localhost/league'
 
+exports.createTeam = function(teamName){
+  // var conString = 'postgresql://OZ:123456@localhost/league';
+	console.log(teamName);
+	var name=teamName;
 	var client = new pg.Client(conString);
-	var sql='create table '+name2+' (id integer UNIQUE, name varchar(20) primary key, team varchar(3),position varchar(3), cost decimal, total_points integer, FOREIGN KEY (id, total_points) references playersdb(id, total_points) on update cascade);'
-
-	client.connect();
-	client.query(sql, (err, res) => {
-	  console.log(err ? err.stack : res) // Hello World!
-
-	client.end()
-	});
-//}
+	 client.connect();
+	 const text = "create table "+name+" (id integer UNIQUE, name varchar(20) primary key, team varchar(3),position varchar(3), cost decimal, total_points integer, FOREIGN KEY (id) references playersdb(id) on update cascade);"+"create function check_budget_"+name+"() returns trigger as '"+ "Begin if NEW.cost +(select sum(cost) from "+name+")>100 then raise notice ''Budget exceeded''; return null; "
++"else return new; end if; end; 'Language 'plpgsql';"+"create trigger budget_trigger"+name+" before insert on "+name+" for each row execute procedure check_budget();";
+	const values = [teamName];
+// callback
+	client.query(text, (err, res) => {
+	  if (err) {
+		console.log(err.stack)
+	  } else {
+		console.log(res)
+    }
+	  client.end();
+	})
 }
